6083c277e87dbaad2edbd2fb0c394e76a4cee1a2
Benhur Davies <benhurdavies@gmail.com>
2020-06-22 22:33:02 +0200

    renamed getLast and getFirst to getLastNode and getFirstNode

--- src/dataStructure/DoublyLinkedList.test.js
+++ src/dataStructure/DoublyLinkedList.test.js
@@ -12,7 +12,7 @@ describe('DoublyLinkedList', () => {
     let removeItem = null;
     const list = new DoublyLinkedList();
     list.addFirst(1);
-    removeItem = list.getFirst();
+    removeItem = list.getFirstNode();
     list.addFirst(2);
     list.addLast(5);
     expect(list.toArray()).toEqual([2, 1, 5]);
@@ -23,32 +23,32 @@ describe('DoublyLinkedList', () => {
 
   it('should give first and last item', () => {
     const list = new DoublyLinkedList();
-    expect(list.getFirst()).toBe(null);
-    expect(list.getLast()).toBe(null);
+    expect(list.getFirstNode()).toBe(null);
+    expect(list.getLastNode()).toBe(null);
 
     list.addFirst(1);
-    expect(list.getFirst().value).toBe(1);
-    expect(list.getLast().value).toBe(1);
+    expect(list.getFirstNode().value).toBe(1);
+    expect(list.getLastNode().value).toBe(1);
 
     list.addFirst(5);
-    expect(list.getFirst().value).toBe(5);
-    expect(list.getLast().value).toBe(1);
+    expect(list.getFirstNode().value).toBe(5);
+    expect(list.getLastNode().value).toBe(1);
 
     list.addFirst(9);
-    expect(list.getFirst().value).toBe(9);
-    expect(list.getLast().value).toBe(1);
+    expect(list.getFirstNode().value).toBe(9);
+    expect(list.getLastNode().value).toBe(1);
 
-    list.remove(list.getLast());
-    expect(list.getFirst().value).toBe(9);
-    expect(list.getLast().value).toBe(5);
+    list.remove(list.getLastNode());
+    expect(list.getFirstNode().value).toBe(9);
+    expect(list.getLastNode().value).toBe(5);
 
-    list.remove(list.getFirst());
-    expect(list.getFirst().value).toBe(5);
-    expect(list.getLast().value).toBe(5);
+    list.remove(list.getFirstNode());
+    expect(list.getFirstNode().value).toBe(5);
+    expect(list.getLastNode().value).toBe(5);
 
-    list.remove(list.getFirst());
-    expect(list.getFirst()).toBe(null);
-    expect(list.getLast()).toBe(null);
+    list.remove(list.getFirstNode());
+    expect(list.getFirstNode()).toBe(null);
+    expect(list.getLastNode()).toBe(null);
   });
 
   it('use case test: 3 element remove last one and insert to first', () => {
@@ -57,19 +57,19 @@ describe('DoublyLinkedList', () => {
     list.addFirst('B');
     list.addFirst('C');
 
-    const node = list.getLast();
+    const node = list.getLastNode();
     list.remove(node);
     expect(list.toArray()).toEqual(['C', 'B']);
 
     list.addFirst(node.value);
     expect(list.toArray()).toEqual(['A', 'C', 'B']);
 
-    const last = list.getLast();
+    const last = list.getLastNode();
     expect(last.prev.value).toBe('C');
     expect(last.prev.prev.value).toBe('A');
 
     list.remove(last);
     list.addFirst(last.value);
-    expect(list.getLast().prev.value).toBe('A');
+    expect(list.getLastNode().prev.value).toBe('A');
   });
 });
