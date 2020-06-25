06f18b8a4e6114fb590a55957c61092399acd01a
Benhur Davies <benhurdavies@gmail.com>
2020-06-25 23:41:49 +0200

    feature implementation of ttl

--- src/engines/TimeToLive.js
+++ src/engines/TimeToLive.js
@@ -4,13 +4,13 @@ import * as hashTableProp from '../hashTableSymbol';
 
 function TimeToLive({ HashTable = DefaultHashTable, defaultTTL } = {}) {
   const store = new HashTable();
-  const timeSeriesIndex = new HashTable();
+  const timePartition = new HashTable();
   const timeIndexInterval = 5 * 60 * 1000; // milliseconds.
 
   let lowestTimePartition = Date.now();
 
   this.add = (key, value, ttl = defaultTTL) => {
-    if (!ttl)
+    if (!ttl && Number.isInteger(ttl))
       throw Error(
         'Expected ttl value. you can have to mention it in add method or mention as defaultTTL at constructor',
       );
@@ -18,7 +18,7 @@ function TimeToLive({ HashTable = DefaultHashTable, defaultTTL } = {}) {
     const expireTTL = Date.now() + ttl;
     const bucket = getTimeBucket(expireTTL);
     bucket.addFirst(key);
-    const tNode = bucket.getFirst();
+    const tNode = bucket.getFirstNode();
     const payload = { value, ttl: expireTTL, tNode };
     store[hashTableProp.add](key, payload);
   };
@@ -27,17 +27,20 @@ function TimeToLive({ HashTable = DefaultHashTable, defaultTTL } = {}) {
     const payload = store[hashTableProp.get](key);
     if (payload) {
       const { ttl, value } = payload;
-      if (checkIfElementExpire(payload)) return undefined;
+      if (checkIfElementExpire({ ttl, key })) return undefined;
       else return value;
     }
     return undefined;
   };
 
   this.has = key => {
-    return store[hashTableProp.has](key);
+    return (
+      store[hashTableProp.has](key) &&
+      !checkIfElementExpire({ ttl: store[hashTableProp.get](key), key })
+    );
   };
 
-  this.remove = (key) => {
+  this.remove = key => {
     if (this.has(key)) {
       const { ttl, tNode } = store[hashTableProp.get](key);
       const timeBucket = getTimeBucket(ttl);
@@ -50,42 +53,96 @@ function TimeToLive({ HashTable = DefaultHashTable, defaultTTL } = {}) {
     return store[hashTableProp.size]();
   };
 
-  function getTimeBucket(expireTTL) {
-    const timeIndex = getTimeIndex({ time: expireTTL, interval:timeIndexInterval });
+  this.runGC = () => {
+    const cleanTo = getBackwardTimeIndex({ time: Date.now(), interval: timeIndexInterval });
+    cleanExpiredBuckets(cleanTo);
 
-    if (timeSeriesIndex[hashTableProp.has](timeIndex)) {
-      return timeSeriesIndex[hashTableProp.get](timeIndex);
+    const nextCleanBucket = getForwardTimeIndex({ time: Date.now(), interval: timeIndexInterval });
+    cleanNotExpiredBucket(nextCleanBucket);
+  };
+
+  function getTimeBucket(expireTTL) {
+    const timeIndex = getForwardTimeIndex({
+      time: expireTTL,
+      interval: timeIndexInterval,
+    });
+
+    if (timePartition[hashTableProp.has](timeIndex)) {
+      return timePartition[hashTableProp.get](timeIndex);
     } else {
       const list = new DoublyLinkedList();
-      timeSeriesIndex[hashTableProp.add](timeIndex, list);
+      timePartition[hashTableProp.add](timeIndex, list);
       return list;
     }
   }
 
-  function checkIfElementExpire({ ttl }) {
+  const checkIfElementExpire = ({ ttl, key }) => {
     if (ttl < Date.now()) {
-      const timeIndex = getTimeIndex({ time: ttl, interval:timeIndexInterval });
-      cleanExpired(timeIndex);
+      const timeIndex = getBackwardTimeIndex({
+        time: ttl,
+        interval: timeIndexInterval,
+      });
+      this.remove(key);
+      cleanExpiredBuckets(timeIndex);
       return true;
     }
     return false;
+  };
+
+  function cleanExpiredBucket(timeIndex) {
+    if (timePartition[hashTableProp.has](timeIndex)) {
+      const tNodes = timePartition[hashTableProp.get](timeIndex);
+      for (const tNode of tNodes) {
+        store[hashTableProp.remove](tNode.value);
+      }
+      timePartition[hashTableProp.remove](timeIndex);
+    }
   }
 
-  function cleanExpired(timeIndex) {
-    const keys = timeSeriesIndex[hashTableProp.get][timeIndex];
-    for (key of keys) {
-      store[hashTableProp.remove](key);
+  function cleanExpiredBuckets(tillTimeIndex) {
+    const cleanFrom = getForwardTimeIndex({
+      time: lowestTimePartition,
+      interval: timeIndexInterval,
+    });
+
+    for (const curTimeIndex of getIndexBetween({
+      from: cleanFrom,
+      to: tillTimeIndex,
+      interval: timeIndexInterval,
+    })) {
+      cleanExpiredBucket(curTimeIndex);
+    }
+    lowestTimePartition = tillTimeIndex;
+  }
+
+  function cleanNotExpiredBucket(timeIndex) {
+    if (timePartition[hashTableProp.has](timeIndex)) {
+      const tNodes = timePartition[hashTableProp.get](timeIndex);
+      for (const { value: key } of tNodes) {
+        const { ttl } = store[hashTableProp.get](key);
+        if (ttl < Date.now()) {
+          store[hashTableProp.remove](key);
+        }
+      }
     }
   }
 }
 
 // time : unix timestamp milliseconds
 // interval : milliseconds (better to be factors of 60  (minutes))
-function getTimeIndex({ time, interval }) {
+function getForwardTimeIndex({ time, interval }) {
   const timeParts = parseInt(time / interval, 10);
-  const forwardIndexTime = timeParts * interval + interval;
-  return forwardIndexTime;
+  return timeParts * interval + interval;
 }
 
-export { getTimeIndex };
+function getBackwardTimeIndex({ time, interval }) {
+  const timeParts = parseInt(time / interval, 10);
+  return timeParts * interval;
+}
+
+function* getIndexBetween({ from, to, interval }) {
+  for (let i = from; i <= to; i += interval) yield i;
+}
+
+export { getForwardTimeIndex, getBackwardTimeIndex, getIndexBetween };
 export default TimeToLive;
--- src/engines/TimeToLive.test.js
+++ src/engines/TimeToLive.test.js
@@ -1,31 +1,67 @@
-import TimeToLive, { getTimeIndex } from './TimeToLive';
+import MockDate from 'mockdate';
 
-describe('TimeToLive (TTL) : getTimeIndex', () => {
+import TimeToLive, {
+  getForwardTimeIndex,
+  getBackwardTimeIndex,
+  getIndexBetween,
+} from './TimeToLive';
+
+const toMinute = val => val * 60 * 1000;
+
+describe('TimeToLive (TTL) : getForwardTimeIndex', () => {
   it('should index time to next upcoming interval', () => {
-    const toMinute = val => val * 60 * 1000;
-
     const date1 = new Date('2020-06-14T03:23:34');
-    expect(getTimeIndex({ time: date1.getTime(), interval: toMinute(5) })).toBe(
+    expect(getForwardTimeIndex({ time: date1.getTime(), interval: toMinute(5) })).toBe(
       new Date('2020-06-14T03:25:00').getTime(),
     );
 
     const date2 = new Date('2020-06-14T03:43:36');
-    expect(
-      getTimeIndex({ time: date2.getTime(), interval: toMinute(10) }),
-    ).toBe(new Date('2020-06-14T03:50:00').getTime());
+    expect(getForwardTimeIndex({ time: date2.getTime(), interval: toMinute(10) })).toBe(
+      new Date('2020-06-14T03:50:00').getTime(),
+    );
 
     const date3 = new Date('2020-06-14T03:43:36');
-    expect(getTimeIndex({ time: date3.getTime(), interval: toMinute(3) })).toBe(
+    expect(getForwardTimeIndex({ time: date3.getTime(), interval: toMinute(3) })).toBe(
       new Date('2020-06-14T03:45:00').getTime(),
     );
+  });
+});
+
+describe('TimeToLive (TTL) : getBackwardTimeIndex', () => {
+  it('should index time to before upcoming interval', () => {
+    const date1 = new Date('2020-06-14T03:23:34');
+    expect(getBackwardTimeIndex({ time: date1.getTime(), interval: toMinute(5) })).toBe(
+      new Date('2020-06-14T03:20:00').getTime(),
+    );
+
+    const date2 = new Date('2020-06-14T03:43:36');
+    expect(getBackwardTimeIndex({ time: date2.getTime(), interval: toMinute(10) })).toBe(
+      new Date('2020-06-14T03:40:00').getTime(),
+    );
+  });
+});
+
+describe('TimeToLive (TTL) : getBackwardTimeIndex', () => {
+  it('should generate index between from and to', () => {
+    const interval = 5 * 60 * 1000;
+    const date1 = new Date('2020-06-14T03:23:34');
+    const date2 = new Date('2020-06-14T03:36:34');
+    const from = getForwardTimeIndex({ time: date1.getTime(), interval });
+    const to = getBackwardTimeIndex({ time: date2.getTime(), interval });
+
+    expect([...getIndexBetween({ from, to, interval })]).toEqual([
+      new Date('2020-06-14T03:25:00').getTime(),
+      new Date('2020-06-14T03:30:00').getTime(),
+      new Date('2020-06-14T03:35:00').getTime(),
+    ]);
   });
 });
 
 describe('TimeToLive', () => {
   it('should have basic cache features', () => {
     const ttlCache = new TimeToLive();
-    ttlCache.add('apple',5,1000);
-    ttlCache.add('orange',2,2000);
+    ttlCache.add('apple', 5, 1000);
+    ttlCache.add('orange', 2, 2000);
     expect(ttlCache.get('apple')).toBe(5);
     expect(ttlCache.has('apple')).toBe(true);
     expect(ttlCache.size()).toBe(2);
@@ -34,5 +70,40 @@ describe('TimeToLive', () => {
     expect(ttlCache.size()).toBe(1);
     expect(ttlCache.get('apple')).toBe(undefined);
     expect(ttlCache.get('orange')).toBe(2);
+  });
+
+  it('should not return expired item', () => {
+    MockDate.set(new Date('2020-06-25T03:25:00'));
+    const ttlCache = new TimeToLive();
+    ttlCache.add('ben', 5, 5000);
+    expect(ttlCache.get('ben')).toBe(5);
+
+    MockDate.set(new Date('2020-06-25T03:26:00'));
+    expect(ttlCache.get('ben')).toBe(undefined);
+    expect(ttlCache.size()).toBe(0);
+
+    ttlCache.add('mango', 10, 180000);
+    ttlCache.add('grape', 33, 660000);
+
+    MockDate.set(new Date('2020-06-25T03:45:00'));
+    expect(ttlCache.get('grape')).toBe(undefined);
+    expect(ttlCache.size()).toBe(0);
+
+    MockDate.reset();
+  });
+
+  it('should clean expired item when runGC calls', () => {
+    MockDate.set(new Date('2020-06-25T03:24:00'));
+    const ttlCache = new TimeToLive();
+
+    ttlCache.add('test1', 5, 180000); // 3mint
+    ttlCache.add('test2', 2, 660000); // 12 mint
+    ttlCache.add('test3', 60, 900000); // 15 mint
+    ttlCache.add('test4', 43, 1080000); // 18 mint
+    expect(ttlCache.size()).toBe(4);
+
+    MockDate.set(new Date('2020-06-25T03:40:00'));
+    ttlCache.runGC();
+    expect(ttlCache.size()).toBe(1);
   });
 });
